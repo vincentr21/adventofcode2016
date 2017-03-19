55b0856b6d8fa9ee18123a774a7cd62717b8d79b
Vincent Ren <vincent.ren21@gmail.com>
2017-03-19 14:34:15 -0400

    complete q7 part 1

--- q7.py
+++ q7.py
@@ -13,12 +13,62 @@ input = input.split('\n')
 print input
 # print len(input)
 
-line = input[1]
+# line = input[0]
+#
+# inner = re.findall(r'\[([a-z]+)\]', line)
+# # outer = re.findall(r'^([a-z]+)|([a-z]+)$|\]([a-z]+)\[', line)
+# # couldn't put these two regexes into one...
+# outer = re.findall(r'(.*?)\[.*?\]', line)
+# last_outer = re.findall(r'\[[a-z]*?\]([a-z]*?)$', line)
 
-inner = re.findall(r'\[([a-z]+)\]', line)
-# outer = re.findall(r'^([a-z]+)|([a-z]+)$|\]([a-z]+)\[', line)
-outer = re.findall(r'(.*?)\[.*?\]', line)
+# appends the last found token by regex into the outer list
+# outer.append(last_outer[0])
 
-print inner
-print outer
+count = 0
+
+def isABBA(str):
+    N = len(str)
+    # print N
+    # print str
+    for i in range(1, N-2):
+        a0 = str[i-1]
+        b0 = str[i]
+        b1 = str[i+1]
+        a1 = str[i+2]
+        # print str[i-1], str[i], str[i+1], str[i+2]
+        if a0 != b0 and a0 == a1 and b0 == b1:
+            return True
+    return False
+
+for line in input:
+    isInvalid = False
+
+    inner = re.findall(r'\[([a-z]+)\]', line)
+    # outer = re.findall(r'^([a-z]+)|([a-z]+)$|\]([a-z]+)\[', line)
+    # couldn't put these two regexes into one...
+    outer = re.findall(r'(.*?)\[.*?\]', line)
+    last_outer = re.findall(r'\[[a-z]*?\]([a-z]*?)$', line)
+    # appends the last found token by regex into the outer list
+    outer.append(last_outer[0])
+
+    for token in inner:
+        if isABBA(token):
+            isInvalid = True
+            break
+
+    if isInvalid:
+        continue
+
+    for token in outer:
+        if isABBA(token):
+            # print "FOuND!"
+            count += 1
+            break
+
+# print inner
+# print outer
+# print last_outer
+
+print count
+
 # =============================================== part 2 ======================================================
